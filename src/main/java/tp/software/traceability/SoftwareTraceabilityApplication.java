8c2deadc07cb0038ba7b2551c6d8fb169cbd3a8f
Hatim Allouane <allouane.hatim.ha11@gmail.com>
2022-12-22 16:51:03 +0100

    set up project

---
+++ src/main/java/tp/software/traceability/SoftwareTraceabilityApplication.java
@@ -0,0 +1,12 @@
+package tp.software.traceability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareTraceabilityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareTraceabilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tp/software/traceability/SoftwareTraceabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.software.traceability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTraceabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

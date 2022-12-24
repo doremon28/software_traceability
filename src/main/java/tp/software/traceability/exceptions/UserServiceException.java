1e154b4683d952a5614c57e1094a3b62b12ed627
Hatim Allouane <allouane.hatim.ha11@gmail.com>
2022-12-24 16:02:26 +0100

    refactor(exception-shared): add custom exceptions and constantes

---
+++ src/main/java/tp/software/traceability/exceptions/UserServiceException.java
@@ -0,0 +1,9 @@
+package tp.software.traceability.exceptions;
+
+public class UserServiceException extends RuntimeException {
+    private static final long serialVersionUID = 9025453784649279609L;
+
+    public UserServiceException(String message) {
+        super(message);
+    }
+}

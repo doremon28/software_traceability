6b38c9d682142773f9c024c6c466c66a20ce1d55
Hatim Allouane <allouane.hatim.ha11@gmail.com>
2022-12-26 16:49:23 +0100

    feat(gui-controller): add controller for product ui

---
+++ src/main/java/tp/software/traceability/ui/gui/controllers/graphics/utils/ProductModel.java
@@ -0,0 +1,26 @@
+package tp.software.traceability.ui.gui.controllers.graphics.utils;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+public class ProductModel {
+    private SimpleLongProperty id;
+    private SimpleStringProperty name;
+    private SimpleDoubleProperty price;
+    private SimpleStringProperty expirationDate;
+
+    public ProductModel(Long id, String name, double price, LocalDate expirationDate) {
+        this.id = new SimpleLongProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.price = new SimpleDoubleProperty(price);
+        this.expirationDate = new SimpleStringProperty(expirationDate.toString());
+    }
+}
